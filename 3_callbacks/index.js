bc2650cd042defc56257e7c7f734dfcc5a62a3c0
claudiaamprimo <claudia.amprimo@gmail.com>
2024-05-02 13:56:08 +0200

    callbacks nivel 2

--- 3_callbacks/index.js
+++ 3_callbacks/index.js
@@ -24,5 +24,45 @@ function calculadora(num1, num2, sumar) {
 function sumar(a,b) {
   return a + b
 }
-let result2 = calcul2ra(3,3,sumar)
+let result2 = calculadora(3,3,sumar)
 console.log(result2)
+
+// _________________ Nivel 2 ___________________
+/* Ejercicio 3: Uso de callbacks en funciones asíncronas: Escribe una función
+esperarISaludar que acepte dos parámetros: un nombre y una función de callback.
+La función debe esperar 2 segundos y entonces invocar la función de callback,
+pasando el nombre como parámetro.*/
+
+function esperarISaludar(nombre, saludo) {
+  setTimeout(() => {
+    console.log(saludo(nombre))
+  }, 2000);
+}
+function saludo(nombre){
+  return `Hola mi nombre es: ${nombre}`
+}
+esperarISaludar("Claudia", saludo)
+
+/* Ejercicio 4: Callbacks con arrays: Escribe una función procesarElements que
+acepte dos parámetros: un array y una función de callback. La función
+procesarElements debe invocar la función de callback por cada elemento del array.*/
+
+array = [2,4,6,8]
+function procesarElements(array, callback) {
+  array.forEach(number => {
+    callback(number)
+  });
+}
+function mostrarArray(number){
+  console.log(`El array contiene: ${number}`)
+  }
+
+procesarElements(array, mostrarArray)
+
+// _________________ Nivel 3 ___________________
+/* Ejercicio 5: Escribe una función procesarCadena que acepte dos parámetros:
+una cadena de caracteres y una función de callback. La función procesarCadena
+debe convertir la cadena a mayúsculas y entonces invocar la función de callback
+con la cadena transformada.*/
+
+
